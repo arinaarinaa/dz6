b068fe1c6cda3886f84e42fe1905f0160e9ca549
arinach <arina.tchukurova@yandex.ru>
2022-09-07 17:06:11 +0300

    add first

---
+++ first/Program.cs
@@ -0,0 +1,28 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь
+
+Console.Write("Введи количество чисел: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[x];
+
+void FillArray (int num)
+{
+for (int i = 0; i < num; i++)
+  {
+    Console.Write($"Еще: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+
+int Result(int[] array1)
+{
+  int count = 0;
+  for (int i = 0; i < array1.Length; i++)
+  {
+    if(array1[i] > 0 ) count ++; 
+  }
+  return count;
+}
+
+FillArray(x);
+Console.WriteLine($"Введено чисел больше 0: {Result(array)} ");
